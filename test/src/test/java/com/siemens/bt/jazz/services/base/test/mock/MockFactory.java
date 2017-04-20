8f8810f00131f429ad5f323850f413429d8d8958
SBI- <pascal.huerlimann@gmail.com>
2017-04-20 17:49:13 +0200

    Deleted unneccessary MockBuilder and just use RestActionBuilder straight away.

--- test/src/test/java/com/siemens/bt/jazz/services/base/test/mock/MockFactory.java
+++ test/src/test/java/com/siemens/bt/jazz/services/base/test/mock/MockFactory.java
@@ -7,6 +7,6 @@ import com.siemens.bt.jazz.services.base.router.factory.ServiceFactory;
 public class MockFactory implements ServiceFactory {
     @Override
     public RestActionBuilder getBuilder() {
-        return new MockBuilder(TestService.class);
+        return new RestActionBuilder(TestService.class);
     }
 }
