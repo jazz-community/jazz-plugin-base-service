781b83417f5d4c2ced4b5336ff23c84a7e45d989
SBI- <pascal.huerlimann@gmail.com>
2017-04-20 14:27:31 +0200

    Corrected package name for example test.

--- test/src/test/java/com/siemens/bt/jazz/services/base/ExampleTest.java
+++ test/src/test/java/com/siemens/bt/jazz/services/base/ExampleTest.java
@@ -1,6 +1,9 @@
-package com.siemens.example;
+package com.siemens.bt.jazz.services.base;
 
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +12,11 @@ public class ExampleTest {
     @Test
     public void testTrue() throws Exception {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testFalse() throws Exception {
+        assertTrue(true);
     }
 }
 
