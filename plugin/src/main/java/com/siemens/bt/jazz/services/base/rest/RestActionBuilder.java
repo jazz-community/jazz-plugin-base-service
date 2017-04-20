78c7cd6ee8b467b9b6b53db31717fdffbe76fcd4
SBI- <pascal.huerlimann@gmail.com>
2017-04-20 17:42:29 +0200

    Fixed javadoc indentation and broken tags.

--- plugin/src/main/java/com/siemens/bt/jazz/services/base/rest/RestActionBuilder.java
+++ plugin/src/main/java/com/siemens/bt/jazz/services/base/rest/RestActionBuilder.java
@@ -10,13 +10,17 @@ import java.lang.reflect.InvocationTargetException;
 
 /**
  * Abstract class for implementing the builder pattern for RestActions.
+ *
  * <p>
- * <p>Classes that extend this class have access to all the possible resources they might need. However, only the
- * required resources can be passed into internal classes to keep them sanitized.</p>
+ *  Classes that extend this class have access to all the possible resources they might need. However, only the
+ *  required resources can be passed into internal classes to keep them sanitized.
+ * </p>
+ *
  * <p>
- * <p>All implementations return an instance of an inner private class with the intended functionality. Classes extending
- * RestActionBuilder are therefore only wrappers for creating instances of actions. This is done for keeping the scope
- * of all the builder variables as tight as possible.</p>
+ *  All implementations return an instance of an inner private class with the intended functionality. Classes extending
+ *  RestActionBuilder are therefore only wrappers for creating instances of actions. This is done for keeping the scope
+ *  of all the builder variables as tight as possible.
+ * </p>
  */
 public class RestActionBuilder {
 
